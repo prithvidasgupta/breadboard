ce4a6c35612b72b5baa1e99fdf4fd47ee6f58fcf
Prithvi <106804681+prithvidasgupta@users.noreply.github.com>
2025-02-20 00:05:19 -0500

    init: start writing assets for the renderer

---
+++ webcomponents/README.md
@@ -0,0 +1,12 @@
+# Dev Notes
+
+Thought about using PixiJS. Trying out SVG.js. Generate SVGs assets and then use them in PixiJS maybe...?
+
+Currently using Lit component as a testbench
+
+```
+python -m http.server 8080
+```
+
+
+Feb 20 2025: Adding an SVG generator component for a breadboard. The size and colors can and should be parameterized.
---
+++ webcomponents/assets/breadboard.js
@@ -0,0 +1,90 @@
+import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2.7.4/core/lit-core.min.js';
+import { unsafeHTML } from 'https://unpkg.com/lit-html@2.7.4/directives/unsafe-html.js';
+
+// https://stackoverflow.com/questions/68614776/using-lit-with-javascript-and-no-build-tools
+// code uses chunks from ca4e.com source code
+
+export class BreadBoard extends LitElement {
+
+  static get properties() {
+    return {
+      power_rail_size: { type: Number },
+      row_size: { type: Number },
+      column_size: { type: Number },
+      hole_matrix: { type: Array },
+    }
+  }
+
+  constructor() {
+    super();
+    // default row size should be 5
+    // default coumn size should be 30
+    this.row_size = 5;
+    this.column_size = 29;
+    this.power_rail = this.column_size;
+    this.hole_matrix = Array(5)
+    for (let i = 0; i < this.row_size; i++) {
+      this.hole_matrix[i] = Array(60)
+    }
+  }
+
+  render() {
+    let element = SVG().size('100%', '100%')
+    let x = 0, y = 0;
+    let hc = null;
+    let hole_colors = ['#000', '#fff']
+    for (let row = 0; row < this.row_size; row++) {
+      for (let column = 0; column < this.column_size; column++) {
+        element.rect(30, 30).fill('#000').move(0 + y, 0 + x);
+        element.rect(20, 20).fill('#fae7ac').move(5 + y, 5 + x);
+        if (column % 2 == 0)
+          hc = hole_colors[0]
+        else
+          hc = hole_colors[1]
+        element.circle(10).fill(hc).move(10 + y, 10 + x);
+        y = (column + 1) * 30
+      }
+      x = (row + 1) * 30;
+      y = 0;
+    }
+    // blue ground holes
+    for (let column = 0; column < this.column_size; column++) {
+      if ((column + 1) % 5 != 0) {
+        element.rect(30, 30).fill('#000').move(0 + y, 0 + x);
+        element.rect(20, 20).fill('#fae7ac').move(5 + y, 5 + x);
+        element.circle(10).fill("#00f").move(10 + y, 10 + x);
+      }
+      else {
+        element.rect(30, 30).fill('#000').move(0 + y, 0 + x);
+        element.rect(20, 30).fill('#fae7ac').move(5 + y, 5 + x);
+      }
+      y = (column + 1) * 30
+    }
+    x = x + 30;
+    y = 0;
+    // red power holes
+    for (let column = 0; column < this.column_size; column++) {
+      if ((column + 1) % 5 != 0) {
+        element.rect(30, 30).fill('#000').move(0 + y, 0 + x);
+        element.rect(20, 20).fill('#fae7ac').move(5 + y, 5 + x);
+        element.circle(10).fill('#f00').move(10 + y, 10 + x);
+      }
+      else {
+        element.rect(30, 30).fill('#000').move(0 + y, 0 + x);
+        element.rect(20, 25).fill('#fae7ac').move(5 + y, x);
+      }
+      y = (column + 1) * 30
+    }
+    let alpha = ['A', 'B', 'C', 'D', 'E', '(-)', '(+)']
+    for (let i = 0; i < alpha.length; i++) {
+      element.text(alpha[i]).move(this.column_size * 30 + 5, 5 + i * 30)
+    }
+    for (let i = 0; i < this.column_size; i++) {
+      element.text(i + 1).move(10 + i * 30, (this.row_size + 2) * 30 + 5)
+    }
+    return html`${unsafeHTML(element.svg())}`
+  }
+
+}
+
+customElements.define('bread-board', BreadBoard);
